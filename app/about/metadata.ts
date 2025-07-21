5daed86c0d36c8b7a50a26ba2003ed95f9c0aa89
Fatkhul Umam <ftkhl.umam22@gmail.com>
2025-07-21 09:55:30 +0700

    feat: create About page layout and metadata

---
+++ app/about/metadata.ts
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tentang Kami | ResepKu",
+  description:
+    "Mengenal lebih dekat dengan tim ResepKu dan perjalanan kami berbagi resep masakan Indonesia terbaik",
+  keywords: [
+    "tentang resepku",
+    "tim resepku",
+    "sejarah resepku",
+    "misi resepku",
+    "kuliner indonesia",
+  ],
+  openGraph: {
+    title: "Tentang Kami | ResepKu",
+    description:
+      "Mengenal lebih dekat dengan tim ResepKu dan perjalanan kami berbagi resep masakan Indonesia terbaik",
+    url: "https://www.resepku.com/about",
+    images: [
+      {
+        url: "/images/about-resepku.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Tim ResepKu",
+      },
+    ],
+  },
+};
